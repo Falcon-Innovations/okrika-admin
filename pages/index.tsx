f02c93738326ab2d9b6f9e5ee62512f0b5bab94f
eldrige <peldriige8@gmail.com>
2023-09-19 20:56:41 +0100

    fix: clear home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,11 @@
 
-import {Fragment} from "react";
-import {ParallaxProvider} from 'react-scroll-parallax';
-import {Hero} from "@/components/home/hero";
-import {HowItWorks} from '@/components/home/how-it-works'
-import {WhyChooseUs} from "@/components/home/why-choose-us";
-import {WhyJoinUs} from "@/components/home/why-join-us";
-import {Footer} from "@/components/shared/footer";
-import {Faq} from "@/components/home/faq";
-import {WhatTalentsSay} from "@/components/home/what-talents-say";
+
 
 export default function Home() {
   return (
-    <Fragment>
-      <ParallaxProvider>
-        <Hero />
-        <HowItWorks />
-        <WhyChooseUs />
-        <WhyJoinUs />
-        <WhatTalentsSay />
-        {/* <Faq /> */}
-        <Footer />
-      </ParallaxProvider>
-    </Fragment>
+    <main>
+      <h1>Okrika Admin</h1>
+    </main>
   );
 }
 
